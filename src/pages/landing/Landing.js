039ce416bd4cac1a1fd175f42712f6a24241efde
Kara X_X <jameszhan02@hotmail.com>
2025-01-21 17:45:38 -0800

    splash screen on going

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,29 +1,40 @@
-import { ProgressBar } from 'react-bootstrap';
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
-
-
-import './Landing.css';
+import { motion } from "motion/react";
+import React, { useEffect } from "react";
+import "./Landing.css";
 
 function Landing() {
-    const [progressBar, setProgressBar] = useState(0);
-    const navigate = useNavigate();
-    useEffect(()=>{
-        const timer = setTimeout(() => {
-            setProgressBar(progressBar+2);
-            if(progressBar > 130){
-                clearTimeout(timer);
-                navigate('/home');
-            }
-        }, 50);
-    })
+  useEffect(() => {}, []);
 
-    return (
-        <div className="landing-container">
-            <div className="loader-txt">Sheng is Building</div>
-            <ProgressBar variant="warning" style={{width:"300px"}} animated now={progressBar} />
-        </div>
-    )
+  const containerVariants = {
+    initial: { opacity: 1 },
+    animate: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 1, // This will create a 1 second delay between children
+      },
+    },
+  };
+
+  return (
+    <motion.div className="splash-screen-container">
+      <motion.div
+        initial={{ width: 0, opacity: 0.3 }}
+        animate={{ width: "fit-content", opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="open-notation"
+      >
+        'Sheng Zhan'
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          className="open-notation-absolute"
+        >
+          ''
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
 }
 
 export default Landing;
