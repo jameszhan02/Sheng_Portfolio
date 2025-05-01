e2bfa2e6577d6df1b7562ea21b3ed8605ec97f4c
Kara X_X <jameszhan02@hotmail.com>
2025-05-01 14:16:21 -0700

    [chore] experiecnes done

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -167,6 +167,7 @@ function Home() {
           </div>
           <div className="experience-title-txt">Experience</div>
         </div>
+        {/* Experience 1 */}
         <div className="experience-content">
           <div className="experience-content-date">JUNE 2022 — PRESENT</div>
           <div className="experience-content-detail">
@@ -199,20 +200,157 @@ function Home() {
             <div className="experience-content-detail-bullets">
               <ul>
                 <li>
-                  Developed and maintained a full-stack web application using
-                  React, TypeScript, and Node.js.
+                  Implemented alert mechanisms for backend APIs and advocated
+                  for an on-call system within the team. This approach uses
+                  multiple metrics as standards, including average API response
+                  time, error rates, third-party service status, server
+                  performance, and overall service health. These improvements
+                  significantly reduced app downtime and enhanced user
+                  experience.
                 </li>
                 <li>
-                  Developed and maintained a full-stack web application using
-                  React, TypeScript, and Node.js.
+                  Redesigned the Java API architecture by advocating for and
+                  implementing Redis caching and asynchronous job processing.
+                  This strategic enhancement significantly reduced API response
+                  times and decreased server costs by $5,000 annually.
                 </li>
                 <li>
-                  Developed and maintained a full-stack web application using
-                  React, TypeScript, and Node.js.
+                  Maintained CI/CD pipelines using GitHub Actions, ensuring
+                  99.9% deployment success rate, automating most of the manual
+                  deployment process.
                 </li>
                 <li>
-                  Developed and maintained a full-stack web application using
-                  React, TypeScript, and Node.js.
+                  Rapidly mastered Android development, delivering a
+                  production-ready feature within a month despite no prior
+                  experience, demonstrating adaptability and strong
+                  problem-solving skills.
+                </li>
+                <li>
+                  Automated accounting workflows by replacing manual data entry
+                  with E2E testing tools, saving the department 15+ hours per
+                  week and reducing data entry errors.
+                </li>
+                <li>
+                  Identified and addressed performance bottlenecks, implementing
+                  targeted indexing strategies and optimizing data filtering.
+                  Successfully reduced a 7-second search to under 1 second,
+                  significantly enhancing efficiency.
+                </li>
+                <li>
+                  Mentored interns, conducting code reviews for each pull
+                  request to ensure high code quality.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        {/* Experience 2 */}
+        <div className="experience-content">
+          <div className="experience-content-date">JAN 2021 — May 2022</div>
+          <div className="experience-content-detail">
+            <div className="experience-content-detail-title">
+              Front-End Developer
+            </div>
+            <div className="company-row">
+              <div className="company-logo">
+                <img src={peacefulMallIcon} alt="Peaceful Mall" />
+              </div>
+              <div className="experience-content-detail-company">
+                <a
+                  href="https://www.peacefulmall.com/en"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Peaceful Mall
+                </a>
+                <div className="company-location"> - Vancouver, BC, Canada</div>
+              </div>
+            </div>
+            <div className="experience-content-detail-description">
+              Peaceful Mall is a comprehensive platform offering a wide range of
+              services, including e-commerce and food delivery. Our mission is
+              to provide users with more convenient services and efficient
+              delivery options.
+            </div>
+            <div className="experience-content-detail-bullets">
+              <ul>
+                <li>
+                  Implemented a payment interface utilizing an event queue to
+                  ensure quick response times and data consistency. This setup
+                  guarantees that any failure at any step triggers a rollback of
+                  changes.
+                </li>
+                <li>
+                  Refactored a large portion of the existing project code using
+                  React Native best practices, significantly improving app
+                  performance by preventing excessive re-render issues.
+                </li>
+                <li>
+                  Developed 10+ reusable UI components in collaboration with the
+                  design team, ensuring consistent branding and 100%
+                  responsiveness across 5+ device types/size.
+                </li>
+                <li>
+                  Redesigned state management with Redux and Context API,
+                  reducing API calls and improving app stability.
+                </li>
+                <li>
+                  Debugged and optimized UI performance, identifying rendering
+                  bottlenecks using React DevTools and Chrome Performance
+                  Profiler.
+                </li>
+                <li>
+                  Identified and addressed performance bottlenecks, implementing
+                  targeted indexing strategies and optimizing data filtering.
+                  Successfully reduced a 7-second search to under 1 second,
+                  significantly enhancing efficiency.
+                </li>
+                <li>
+                  Well-versed in Agile and Iterative SDLC methodologies. Used
+                  Asana for project tracking and sprint management, enhancing
+                  team collaboration. Maintained clear documentation in Notion,
+                  making technical information easily accessible.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        {/* Experience 3 */}
+        <div className="experience-content">
+          <div className="experience-content-date">MAR 2021 — DEC 2021</div>
+          <div className="experience-content-detail">
+            <div className="experience-content-detail-title">
+              Full-Stack Developer
+            </div>
+            <div className="company-row">
+              <div className="company-logo">
+                <img src={novaInteractionLogo} alt="Nova Interaction" />
+              </div>
+              <div className="experience-content-detail-company">
+                <a href="https://no-va.ca/" target="_blank" rel="noreferrer">
+                  Nova Interaction
+                </a>
+                <div className="company-location"> - Richmond, BC, Canada</div>
+              </div>
+            </div>
+
+            <div className="experience-content-detail-description">
+              Nova Interaction is a software development consultancy. The
+              dynamic work environment and rigorous code reviews have provided a
+              strong foundation for my career. Engaging directly with clients
+              has enhanced my communication and requirements analysis skills.
+            </div>
+            <div className="experience-content-detail-bullets">
+              <ul>
+                <li>
+                  Developed and deployed 3 full-stack web applications, using
+                  React, Node.js, and PostgreSQL, delivering projects ahead of
+                  schedule while ensuring 100% uptime post-launch.
+                </li>
+                <li>
+                  Troubleshoot and resolve critical backend API issues, and
+                  improving system stability through log monitoring and
+                  automated testing
                 </li>
               </ul>
             </div>
