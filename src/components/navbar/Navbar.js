4d53801c8e74dec51caadc18f47ba2357d305986
Kara X_X <jameszhan02@hotmail.com>
2025-05-01 16:01:36 -0700

    [chore] flip coin hover in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,17 @@
-import logo from "../../images/personal_logo.jpg";
+import avator from "../../images/avatar.jpg";
+import realAvatar from "../../images/real_avatar.jpg";
 import "./Navbar.css";
 
 function Navbar() {
   return (
     <div className="my-navbar">
       <div className="my-logo">
-        <img src={logo} alt="logo" />
+        <div className="flip-container">
+          <div className="flipper">
+            <img src={realAvatar} alt="logo" className="back" />
+            <img src={avator} alt="logo" className="front" />
+          </div>
+        </div>
       </div>
       <ul>
         <li>Projects</li>
